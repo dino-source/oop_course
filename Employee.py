9f8a3a08ccd5b8fb4fe9e8ef01f29cd22573874f
Dino Source <dinosource.cpp@gmail.com>
2023-04-05 21:51:05 +0400

    Add __repr__ method implementation

--- Employee.py
+++ Employee.py
@@ -1,15 +1,24 @@
 class Employee:
-    def __init__(self, name, age, position, salary):
+    def __init__(self, name, age, position, salary) -> None:
         self.name = name
         self.age = age
         self.position = position
         self.salary = salary
 
-    def print(self):
-        print(f"{self.name} ({self.age}): {self.position} [$", "%.2f" % self.salary, "]", sep='')
-
     def __str__(self) -> str:
         return f"{self.name} ({self.age}): {self.position} [$" + "%.2f" % self.salary + "]"
+    
+    def __repr__(self) -> str:
+        return str(
+            'Employee.Employee(' +
+            '\"' + str(self.name) + '\"' + ', ' +
+            str(self.age) + ', ' +
+            '\"' + str(self.position) + '\"' + ', ' +
+            str(self.salary) + ')'
+        )
+
+    def print(self):
+        print(f"{self.name} ({self.age}): {self.position} [$", "%.2f" % self.salary, "]", sep='')
 
     def increase_salary(self, percent):
         self.salary += self.salary * (percent/100)
--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import Employee
 
 
 e1 = Employee.Employee("John Smith", 38, "developer", 1200.0)
-e1.print()
+##e1.print()
 
 e2 = Employee.Employee("Jane Croft", 44, "tester", 1000.0)
-print(e2)
+#print(e2)
 
 e2.increase_salary(30)
-print(e2)
+#print(e2)
+
+print(eval(repr(e1)))
