7edad40c549dc6bb8613d4563c07d288e9e8f6c4
Dino Source <dinosource.cpp@gmail.com>
2023-04-05 22:40:37 +0400

    Make salary private

--- Employee.py
+++ Employee.py
@@ -3,10 +3,10 @@ class Employee:
         self.name = name
         self.age = age
         self.position = position
-        self.salary = salary
+        self.__salary = salary
 
     def __str__(self) -> str:
-        return f"{self.name} ({self.age}): {self.position} [$" + "%.2f" % self.salary + "]"
+        return f"{self.name} ({self.age}): {self.position} [$" + "%.2f" % self.__salary + "]"
     
     def __repr__(self) -> str:
         return str(
@@ -14,14 +14,24 @@ class Employee:
             '\"' + str(self.name) + '\"' + ', ' +
             str(self.age) + ', ' +
             '\"' + str(self.position) + '\"' + ', ' +
-            str(self.salary) + ')'
+            str(self.__salary) + ')'
         )
     
     def __add__(self, other):
         return [self, other]
+    
+    def get_salary(self) -> float:
+        return self.__salary
+    
+    def set_salary(self, new_salary):
+        minimum_wage = 1000
+        if new_salary < minimum_wage:
+            raise ValueError("Minimum wage is $1000")
+        else:
+            self.__salary = new_salary
 
     def print(self):
-        print(f"{self.name} ({self.age}): {self.position} [$", "%.2f" % self.salary, "]", sep='')
+        print(f"{self.name} ({self.age}): {self.position} [$", "%.2f" % self.__salary, "]", sep='')
 
     def increase_salary(self, percent):
-        self.salary += self.salary * (percent/100)
+        self.__salary += self.__salary * (percent/100)
--- main.py
+++ main.py
@@ -2,16 +2,22 @@ import Employee
 
 
 e1 = Employee.Employee("John Smith", 38, "developer", 1200.0)
-##e1.print()
+e1.print()
 
 e2 = Employee.Employee("Jane Croft", 44, "tester", 1000.0)
-#print(e2)
+print(e2)
 
 e2.increase_salary(30)
-#print(e2)
+print(e2)
 
 print(e1)
 print(eval(repr(e1)))
 
-e3 = e1 + e2
-print(e3)
+employees = e1 + e2
+print(employees)
+
+# e2.set_salary(900)
+e2.set_salary(1400)
+print(e2.name, " has $", e2.get_salary(), " monthly salary.", sep='')
+
+print(e2.__salary)
