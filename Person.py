3266fff2c42a0bcecfa18d108827382ecca4ec8b
Dino Source <dinosource.cpp@gmail.com>
2023-04-06 09:13:21 +0400

    Improve Person class

--- Person.py
+++ Person.py
@@ -2,9 +2,32 @@ class Person:
     def __init__(self, first_name, last_name, id) -> None:
         self.__first_name = first_name
         self.__last_name = last_name
-        self.__id = id
+        self.id = id
 
     @property
     def full_name(self):
         return self.first_name + ' ' + self.last_name
 
+    @property
+    def first_name(self):
+        pass
+
+    @first_name.getter
+    def first_name(self):
+        return self.__first_name
+    
+    @first_name.setter
+    def first_name(self, new_first_name):
+        self.__first_name = new_first_name
+
+    @property
+    def last_name(self):
+        pass
+
+    @last_name.getter
+    def last_name(self):
+        return self.__last_name
+    
+    @last_name.setter
+    def last_name(self, new_last_name):
+        self.__last_name = new_last_name
---
+++ person_main.py
@@ -0,0 +1,8 @@
+import Person
+
+
+p1 = Person.Person("Roger", "Glover", 1234)
+
+p1.first_name = "Glen"
+p1.last_name = "Huges"
+print(p1.full_name)
