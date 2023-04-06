7616745d49b25f9f78992b6a5cee0834f9396cea
Dino Source <dinosource.cpp@gmail.com>
2023-04-06 17:51:08 +0400

    Add mixin class example

--- slots.py
+++ slots.py
@@ -1,14 +1,22 @@
-class Developer:
-    __slots__ = ("name", "age", "salary", "framework")
+class SlotInspectorMixin:
+    def has_slots(self):
+        return hasattr(self, "__slots__")
+    
+    
+class Employee():
+    __slots__ = ("name", "age", "salary")
 
-    def __init__(self, name, age, salary, framework) -> None:
+    def __init__(self, name, age, salary) -> None:
         self.name = name
         self.age = age
         self.salary = salary
-        self.framework = framework
 
-    def has_slots(self):
-        return hasattr(self, "__slots__")
+class Developer(SlotInspectorMixin, Employee):
+    __slots__ = ("framework", )
+
+    def __init__(self, name, age, salary, framework='') -> None:
+        super().__init__(name, age, salary)
+        self.framework = framework
 
 employee1 = Developer("Juliana Craine", 38, 1000, "Flask")
 print(employee1.__slots__)
