2a7937c4f32c4eb86823f201ba5ceefc5864525e
Dino Source <dinosource.cpp@gmail.com>
2023-04-06 22:39:45 +0400

    Use new Python feature - dataclasses to follow KISS principle

--- data_classes.py
+++ data_classes.py
@@ -1,23 +1,18 @@
+from dataclasses import dataclass
+
+
+@dataclass
 class Project:
-    def __init__(self, name, payment, client) -> None:
-        self.name = name
-        self.payment = payment
-        self.client = client
+    name: str
+    payment: int
+    client: str
 
-    def __repr__(self) -> str:
-        return (
-            f"Project" +
-            f"(name={repr(self.name)}, " + 
-            f"payment={repr(self.payment)}, " +
-            f"client={repr(self.client)})"
-        )
-
+@dataclass
 class Employee:
-    def __init__(self, name, age, salary, project) -> None:
-        self.name = name
-        self.age = age
-        self.salary = salary
-        self.project = project
+    name: str
+    age: int
+    salary: float
+    project: Project
 
 p = Project("Django app", 20000, "Globomantics")
 e = Employee("Ji-Soo", 38, 1000, p)
