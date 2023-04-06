316b58fc05c52ef520345972412a38ca5a31875b
Dino Source <dinosource.cpp@gmail.com>
2023-04-06 13:20:44 +0400

    Add one more attribute (field) to Developer class

--- Developer.py
+++ Developer.py
@@ -2,6 +2,10 @@ import Employee2
 
 
 class Developer(Employee2.Employee):
+    def __init__(self, name, age, salary, framework) -> None:
+        super().__init__(name, age, salary)
+        self.framework = framework
+
     def increase_salary(self, percent, bonus=0):
         super().increase_salary(percent)
         self.salary += bonus
--- employee2_main.py
+++ employee2_main.py
@@ -10,9 +10,10 @@ employee1.increase_salary(percent)
 print(f"We increased {employee1.name}'s salary by {percent}%")
 print(f"Current conditions for {employee1.name} in our company:\n\t", employee1, "\n")
 
-employee2 = Developer.Developer("Anton Golovin", 30, 7000)
+employee2 = Developer.Developer("Anton Golovin", 30, 7000, "Qt")
 print(f"Starting conditions for {employee2.name} in our company:\n\t", employee2)
 employee2.implement_feature()
+print(f"{employee2.name} uses {employee2.framework} in his day-to-day work.")
 monthly_bonus = 300
 percent = 10
 employee2.increase_salary(percent, monthly_bonus)
