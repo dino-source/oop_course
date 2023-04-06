41f794387acb1a06f79ad2b6da9aad0bbb2c83fc
Dino Source <dinosource.cpp@gmail.com>
2023-04-06 13:09:17 +0400

    Add super() method usage to follow DRY principle

--- Developer.py
+++ Developer.py
@@ -3,7 +3,7 @@ import Employee2
 
 class Developer(Employee2.Employee):
     def increase_salary(self, percent, bonus=0):
-        self.salary += self.salary * (percent/100)
+        super().increase_salary(percent)
         self.salary += bonus
 
     def implement_feature(self):
